e3c90e640aa993cc095ddf93d44366dcca3fe32e
karam <karamkaku2000@gmail.com>
2021-09-11 01:08:39 +0200

    add login routes

--- server/auth/route.js
+++ server/auth/route.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {freelancerSignup,clientSignup} = require("./controllers/signup");
 const {freelancerEmailVerification,clientEmailVerification} = require("./controllers/emailVerification");
+const {freelancerLogin,clientLogin} = require("./controllers/login");
+
 router.post("/signup/freelancer",freelancerSignup)
 router.get("/signup/freelancer/:email/:code",freelancerEmailVerification)
-
 router.post("/signup/client",clientSignup)
 router.get("/signup/client/:email/:code",clientEmailVerification)
+
+router.post("/login/freelancer",freelancerLogin)
+router.post("/login/client",clientLogin)
 module.exports = router ;
