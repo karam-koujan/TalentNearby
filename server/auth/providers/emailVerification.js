9ddf47d86f99af36e692a856132f714caffcb3e1
karam <karamkaku2000@gmail.com>
2021-09-12 09:05:01 +0200

    fix an tokenKey typo

--- server/auth/providers/emailVerification.js
+++ server/auth/providers/emailVerification.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
-const {authToken,ownerEmail,ownerPassword} = require("./signup");
-const {StatusError} = require("../../utils/errors/statusError");
+const {tokenKey,ownerEmail,ownerPassword} = require("../../config/keys");
+const {AuthorizationError} = require("../../utils/errors/authorizationError");
 exports.sendVerificationEmailCode = async (email,verificationLink)=>{
-    const code = jwt.sign({email},authToken);
+    const code = jwt.sign({email},tokenKey);
     
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",        
@@ -21,7 +21,7 @@ exports.sendVerificationEmailCode = async (email,verificationLink)=>{
         subject: "user email verification", 
         text: "welcome to my project we are glad that you join to this project for security purposes please click the link to verify our account", // plain text body
         html: `
-        <h1>verify Email</h1>
+        <h1 style="color:"#19BD33";textAlign:center;">verify Email</h1>
         <p>welcome to my project we are glad that you join to this project for security purposes please click the link to verify our account</p>
         <a href="${verificationLink}/${email}/${code}">click the link to verify email</a>
         `  
@@ -30,12 +30,13 @@ exports.sendVerificationEmailCode = async (email,verificationLink)=>{
 }
 
 exports.emailVerificationProvider = async(email,code,Model)=>{
-  const user = jwt.verify(code,authToken)
+  const user = jwt.verify(code,tokenKey)
+  console.log(user)
   return new Promise(async(resolve,reject)=>{
     if(user.email===email){
           await Model.findOneAndUpdate({email},{active:true})
           resolve()
         }
-    reject(new StatusError("the email verification is denied try again").forbidden())
+    reject(new AuthorizationError("the email verification is denied try again"))
   })
 }
