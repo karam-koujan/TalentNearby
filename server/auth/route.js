69262221cde791e68d53cdf644e43c22813ca414
karam <karamkaku2000@gmail.com>
2021-09-13 10:38:19 +0200

    add forgotpassword routes

--- server/auth/route.js
+++ server/auth/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {freelancerSignup,clientSignup} = require("./controllers/signup");
 const {freelancerEmailVerification,clientEmailVerification} = require("./controllers/emailVerification");
 const {freelancerLogin,clientLogin} = require("./controllers/login");
-
+const {sendResetPasswordCode,codeVerification} = require("./controllers/forgotPassword");
 router.post("/signup/freelancer",freelancerSignup)
 router.get("/signup/freelancer/:email/:code",freelancerEmailVerification)
 router.post("/signup/client",clientSignup)
@@ -11,4 +11,7 @@ router.get("/signup/client/:email/:code",clientEmailVerification)
 
 router.post("/login/freelancer",freelancerLogin)
 router.post("/login/client",clientLogin)
+
+router.post("/forgotPassword/sendResetPasswordCode",sendResetPasswordCode)
+router.post("/forgotPassword/codeVerification",codeVerification)
 module.exports = router ;
