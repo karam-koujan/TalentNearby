1a0f88f0d8da2774af3c436f6b7b283362b198c8
karam <karamkaku2000@gmail.com>
2021-09-17 17:48:17 +0200

    add sendEmailVerification route

--- server/auth/route.js
+++ server/auth/route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {freelancerSignup,clientSignup} = require("./controllers/signup");
-const {freelancerEmailVerification,clientEmailVerification} = require("./controllers/emailVerification");
+const {freelancerEmailVerification,clientEmailVerification,sendClientEmailVerification,sendFreelancerEmailVerification} = require("./controllers/emailVerification");
 const {freelancerLogin,clientLogin} = require("./controllers/login");
 const {sendResetPasswordCode,codeVerification} = require("./controllers/forgotPassword");
 router.post("/signup/freelancer",freelancerSignup)
@@ -14,4 +14,8 @@ router.post("/login/client",clientLogin)
 
 router.post("/forgotPassword/sendResetPasswordCode",sendResetPasswordCode)
 router.post("/forgotPassword/codeVerification",codeVerification)
+
+router.post('/sendEmailVerification/freelancer',sendFreelancerEmailVerification)
+router.post('/sendEmailVerification/client',sendClientEmailVerification)
+
 module.exports = router ;
