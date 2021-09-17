4c65df60289b49b185fa32c481607f16ee954f08
karam <karamkaku2000@gmail.com>
2021-09-17 17:49:11 +0200

    add sendEmailVerification controller

--- server/auth/controllers/emailVerification.js
+++ server/auth/controllers/emailVerification.js
@@ -1,6 +1,21 @@
-const {emailVerificationProvider} = require("../providers/emailVerification");
+const {emailVerificationProvider,sendVerificationEmailCode} = require("../providers/emailVerification");
 const Freelancer = require("../../model/Freelancer");
 const Client = require("../../model/Client");
+
+
+exports.sendFreelancerEmailVerification = async(req,res,next)=>{
+     const {email} = req.body;
+   try{
+        await sendVerificationEmailCode(email,"http://localhost:8080/api/auth/signup/freelancer")
+        return res.json({
+             message:"email has been sent",
+             error:false
+        })
+   }catch(err){
+        next(err)
+   }
+}
+
 exports.freelancerEmailVerification = async(req,res,next)=>{
   const {email,code}  = req.params;
     try{
@@ -15,6 +30,18 @@ exports.freelancerEmailVerification = async(req,res,next)=>{
 }
 
 
+exports.sendClientEmailVerification = async(req,res,next)=>{
+     const {email} = req.body;
+     try{
+          await sendVerificationEmailCode(email,"http://localhost:3000/api/auth/signup/freelancer")
+          return res.json({
+               message:"email has been sent",
+               error:false
+          })
+     }catch(err){
+          next(err)
+     }
+}
 exports.clientEmailVerification = async(req,res,next)=>{
     const {email,code}  = req.params;
       try{
