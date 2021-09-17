07e75f6a636f97eb638f609d96fdfb8981b6a89e
karam <karamkaku2000@gmail.com>
2021-09-18 01:38:06 +0200

    add forgot password pages

---
+++ client/pages/auth/forgotpassword/client.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import ForgotPassword from "../../../features/auth/components/forgotPassword";
+import {MainHeading, Wrapper} from "../../../features/auth/templates";
+
+const Index = ()=>{
+    return(
+       <>
+       <Head>
+           <title>
+               Talent | forgot password 
+           </title>
+       </Head>
+       <Wrapper style={{'width':'40%','margin':'0 auto'}}>
+       <MainHeading style={{'marginTop':'4rem'}}>
+           Forgot Password
+       </MainHeading>
+       <ForgotPassword/>
+       </Wrapper>
+       </>
+    )
+}
+
+export default Index ;
