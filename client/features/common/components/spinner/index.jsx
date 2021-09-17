2e142b79dbe1b378202d85b6a6404966cda90a2c
karam <karamkaku2000@gmail.com>
2021-09-17 14:21:51 +0200

    add spinner

---
+++ client/features/common/components/spinner/index.jsx
@@ -0,0 +1,11 @@
+import Styles from "./style.module.css"
+
+
+
+const Spinner = ()=>{
+    return(
+        <div className={Styles.ldsRing}><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default Spinner
