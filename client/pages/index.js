800b3fd4a85f155014cbe1d5c5d5bed16762a2e1
karam <karamkaku2000@gmail.com>
2021-09-23 19:12:06 +0200

    add googleMap api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,66 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import Layout from '../features/common/layout/components/';
+import {useFetchQuery} from "../hooks/useFetchQuery"; 
+import UserPosition from "../features/userPosition";
 
+const  Index = ()=> {
+  const [geolocation,setGeoLocation] = React.useState(undefined)
+  const {isLoading,data} = useFetchQuery("user","http://localhost:8080/api/profile/")    
+  const defaultOptions = {
+    fullscreenControl: false,
+  } 
+ React.useEffect(()=>{
+    
+    navigator.geolocation.getCurrentPosition((position)=>{
+      if(!isLoading && data.user.longitude!==position.coords.longitude&&data.user.latitude!==position.coords.latitude ){
+          if(data.user.longitude||data.user.latitude){
+            return setGeoLocation({
+             longitude:data.user.longitude,
+             latitude:data.user.latitude
+           })
+          }
+      }
+        return  setGeoLocation({
+           longitude:position.coords.longitude,
+           latitude:position.coords.latitude
+         })
 
-export default function Home() {
-  return (
-    <div>
-     start coding soon....
-    </div>
-  )
+    })
+    return ()=>setGeoLocation(undefined)
+  },[isLoading,data])
+
+    if(!isLoading && geolocation ===undefined){
+          return(
+          <div>
+             allow the browser to take geolocation position 
+          </div>
+          )
+    }
+    return (
+       isLoading?<div>isLoading....</div>:(
+         <Layout data={data.user}>
+       <div style={{ height: '100vh', width: '100%' }}>   
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyATBu4y1OPMu1ctdhBFvBy3L1XecgDyG1k" }}
+            defaultCenter={{lat:geolocation.latitude,lng:geolocation.longitude}}
+            defaultZoom={14}
+            options={defaultOptions} 
+          >
+            <UserPosition
+              lat={geolocation.latitude}
+              lng={geolocation.longitude}
+              userName={data.user.userName}
+              profileImg={data.user.profileImg}
+              isProfile={false}
+            />
+          </GoogleMapReact>
+        </div>
+        </Layout>
+     )
+     
+    )
+  
 }
+
+export default Index;
