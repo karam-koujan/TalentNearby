5d0fd6a3401787fe5ded3435eff529c9338f083d
karam <karamkaku2000@gmail.com>
2021-09-25 01:07:39 +0200

    add userCard

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,43 +2,48 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import Layout from '../features/common/layout/components/';
 import {useFetchQuery} from "../hooks/useFetchQuery"; 
-import UserPosition from "../features/userPosition";
-import ProfileInfoCard from "../features/profile/components/profileInfoCard";
+import UserPosition from "../features/userPosition/components";
+import UserCard from "../features/userPosition/components/userCard";
 const  Index = ()=> {
-  const [geolocation,setGeoLocation] = React.useState(undefined);
+  const options = {fullscreenControl: false ,disableDoubleClickZoom:true,clickableIcons: false}
+  const [newGeolocation,setNewGeoLocation] = React.useState(undefined);
+  const [defaultGeolocation,setDefaultGeoLocation] = React.useState(undefined);
+  const [enableMapClick,setEnableMapClick] = React.useState(false);
+  const [showNewUserPosition,setShowNewUserPosition] = React.useState(false);
   const {isLoading,data} = useFetchQuery("user","http://localhost:8080/api/profile/");    
-  const defaultOptions = {
-    fullscreenControl: false,
-  } 
- React.useEffect(()=>{
-    
+ 
+ React.useEffect(()=>{  
     navigator.geolocation.getCurrentPosition((position)=>{
       if(!isLoading && data.user.longitude!==position.coords.longitude&&data.user.latitude!==position.coords.latitude ){
           if(data.user.longitude||data.user.latitude){
-            return setGeoLocation({
+            return setDefaultGeoLocation({
              longitude:data.user.longitude,
              latitude:data.user.latitude
            })
           }
       }
-        return  setGeoLocation({
+        return  setDefaultGeoLocation({
            longitude:position.coords.longitude,
            latitude:position.coords.latitude
          })
 
     })
-    return ()=>setGeoLocation(undefined)
+    return ()=>setDefaultGeoLocation(undefined)
   },[isLoading,data])
 
   const handleClick = ({lng,lat})=>{
-       setGeoLocation({
-        longitude:lng,
-        latitude:lat
-      })
+     setShowNewUserPosition(true)
+     if(!enableMapClick){
+       setNewGeoLocation({
+          longitude:lng,
+          latitude:lat
+        })
+
+     } 
   }  
-  
-   
-  if(!isLoading && geolocation ===undefined){
+  const handleOnChildEnter = ()=>setEnableMapClick(true)
+  const handleOnChildLeave = ()=>setEnableMapClick(false)
+  if(!isLoading && defaultGeolocation === undefined){
           return(
           <div>
              allow the browser to take geolocation position 
@@ -50,19 +55,21 @@ const  Index = ()=> {
          <Layout data={data.user}>
        <div style={{ height: '100vh', width: '100%' }}>   
           <GoogleMapReact
-            bootstrapURLKeys={{ key:  }}
-            defaultCenter={{lat:geolocation.latitude,lng:geolocation.longitude}}
+            bootstrapURLKeys={{ key:"AIzaSyATBu4y1OPMu1ctdhBFvBy3L1XecgDyG1k"  }}
+            defaultCenter={{lat:defaultGeolocation.latitude,lng:defaultGeolocation.longitude}}
             defaultZoom={17}
-            options={defaultOptions} 
-            onClick = {handleClick}
-            onChildClick={()=>console.log("click")}
+            options={options} 
+            onClick={handleClick}
+           yesIWantToUseGoogleMapApiInternals 
           >
+            {showNewUserPosition?<UserCard lat={newGeolocation.latitude} lng={newGeolocation.longitude} userName={data.user.userName} onMouseEnter={handleOnChildEnter} onMouseLeave={handleOnChildLeave}   profileImg={data.user.profileImg}/>:null }
             <UserPosition
-              lat={geolocation.latitude}
-              lng={geolocation.longitude}
+              lat={data.user.latitude}
+              lng={data.user.longitude}
               userName={data.user.userName}
               profileImg={data.user.profileImg}
               isProfile={false}
+             onMouseEnter={handleOnChildEnter} onMouseLeave={handleOnChildLeave} 
               />
             
 
