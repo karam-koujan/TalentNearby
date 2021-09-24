305afbb5a64fd9cb8a6052da00e5af29206fb4ad
karam <karamkaku2000@gmail.com>
2021-09-24 02:07:13 +0200

    fix some bugs

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,10 +3,10 @@ import GoogleMapReact from 'google-map-react';
 import Layout from '../features/common/layout/components/';
 import {useFetchQuery} from "../hooks/useFetchQuery"; 
 import UserPosition from "../features/userPosition";
-
+import ProfileInfoCard from "../features/profile/components/profileInfoCard";
 const  Index = ()=> {
-  const [geolocation,setGeoLocation] = React.useState(undefined)
-  const {isLoading,data} = useFetchQuery("user","http://localhost:8080/api/profile/")    
+  const [geolocation,setGeoLocation] = React.useState(undefined);
+  const {isLoading,data} = useFetchQuery("user","http://localhost:8080/api/profile/");    
   const defaultOptions = {
     fullscreenControl: false,
   } 
@@ -30,7 +30,15 @@ const  Index = ()=> {
     return ()=>setGeoLocation(undefined)
   },[isLoading,data])
 
-    if(!isLoading && geolocation ===undefined){
+  const handleClick = ({lng,lat})=>{
+       setGeoLocation({
+        longitude:lng,
+        latitude:lat
+      })
+  }  
+  
+   
+  if(!isLoading && geolocation ===undefined){
           return(
           <div>
              allow the browser to take geolocation position 
@@ -42,10 +50,12 @@ const  Index = ()=> {
          <Layout data={data.user}>
        <div style={{ height: '100vh', width: '100%' }}>   
           <GoogleMapReact
-            bootstrapURLKeys={{ key: "AIzaSyATBu4y1OPMu1ctdhBFvBy3L1XecgDyG1k" }}
+            bootstrapURLKeys={{ key:  }}
             defaultCenter={{lat:geolocation.latitude,lng:geolocation.longitude}}
-            defaultZoom={14}
+            defaultZoom={17}
             options={defaultOptions} 
+            onClick = {handleClick}
+            onChildClick={()=>console.log("click")}
           >
             <UserPosition
               lat={geolocation.latitude}
@@ -53,7 +63,9 @@ const  Index = ()=> {
               userName={data.user.userName}
               profileImg={data.user.profileImg}
               isProfile={false}
-            />
+              />
+            
+
           </GoogleMapReact>
         </div>
         </Layout>
