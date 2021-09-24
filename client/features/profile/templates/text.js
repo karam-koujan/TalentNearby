a358a6c1a3d9934fda3f2af94964880f0050e377
karam <karamkaku2000@gmail.com>
2021-09-25 01:08:31 +0200

    remove userCard

--- client/features/profile/templates/text.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-
-export const UserName = styled.p`
-  color:${props=>props.theme.primary}; 
-  font-size:1.3rem;
-  letter-spacing:1px;
-  text-align:center;
-`
