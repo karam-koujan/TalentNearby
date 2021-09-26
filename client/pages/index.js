022d97be78810446390ae5bc1e91f7c8ab58ddd1
karam <karamkaku2000@gmail.com>
2021-09-27 00:49:34 +0200

    add handleHideCard

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,7 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import Layout from '../features/common/layout/components/';
 import {useFetchQuery} from "../hooks/useFetchQuery"; 
 import UserPosition from "../features/userPosition/components";
-import UserCard from "../features/userPosition/components/userCard";
+import ProfileCard from "../features/userPosition/components/profileCard";
 const  Index = ()=> {
   const options = {fullscreenControl: false ,disableDoubleClickZoom:true,clickableIcons: false}
   const [newGeolocation,setNewGeoLocation] = React.useState(undefined);
@@ -32,15 +32,21 @@ const  Index = ()=> {
   },[isLoading,data])
 
   const handleClick = ({lng,lat})=>{
-     setShowNewUserPositionCard(true)
-     if(!disableMapClick){
+    if(!disableMapClick){
+       setShowNewUserPositionCard(true)
        setNewGeoLocation({
           longitude:lng,
           latitude:lat
         })
 
      } 
-  }  
+  } 
+  const handleCloseCard = (state,setState)=>{
+    return ()=>{
+      setState(false)
+      setDisableMapClick(false)
+    }
+  } 
   const handleOnChildEnter = ()=>setDisableMapClick(true)
   const handleOnChildLeave = ()=>setDisableMapClick(false)
   if(!isLoading && defaultGeolocation === undefined){
@@ -62,16 +68,16 @@ const  Index = ()=> {
             onClick={handleClick}
            yesIWantToUseGoogleMapApiInternals 
           >
-            {showNewUserPositionCard?<UserCard lat={newGeolocation.latitude} lng={newGeolocation.longitude} userName={data.user.userName} onMouseEnter={handleOnChildEnter} onMouseLeave={handleOnChildLeave}   profileImg={data.user.profileImg}/>:null }
+            {showNewUserPositionCard?<ProfileCard handleCloseCard={handleCloseCard(showNewUserPositionCard,setShowNewUserPositionCard)} lat={newGeolocation?newGeolocation.latitude:null} lng={newGeolocation?newGeolocation.longitude:null} data={data.user} onMouseEnter={handleOnChildEnter} onMouseLeave={handleOnChildLeave}   />:null }
+          
             <UserPosition
               lat={data.user.latitude}
               lng={data.user.longitude}
-              userName={data.user.userName}
-              profileImg={data.user.profileImg}
+             data={data.user}
               isProfile={false}
+              handleCloseCard={handleCloseCard}
              onMouseEnter={handleOnChildEnter} onMouseLeave={handleOnChildLeave} 
               />
-            
 
           </GoogleMapReact>
         </div>
