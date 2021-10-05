21dc9b5191a7c456c9341b6f591056dd924fc4a4
karam <karamkaku2000@gmail.com>
2021-10-05 04:59:46 +0200

    add rateUser logic

--- client/features/common/components/ratingStars/index.jsx
+++ client/features/common/components/ratingStars/index.jsx
@@ -1,8 +1,9 @@
+import RateUser from "./rateUser";
 import {Wrapper,Star} from "./styles";
 
-const RatingStars = ({rating})=>{
-        return(
-     <Wrapper>
+const RatingStars = ({rating,enableRating,handleRate,handleClick,...props})=>{
+        return(enableRating?<RateUser handleRate={handleRate}/>:
+     <Wrapper onClick={handleClick}>
          <Star className="fa fa-star" checked={1<=rating}/>
          <Star className="fa fa-star"  checked={2<=rating}/>
          <Star className="fa fa-star"  checked={3<=rating}/>
---
+++ client/features/common/components/ratingStars/rateUser.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import {Wrapper,Star} from "./styles";
+
+
+
+
+
+
+const RateUser = ({handleRate,...props})=>{
+ const [rating,setRating] = React.useState(0)
+ const [isClicked,setIsClicked] = React.useState(false)
+ const handleHover = (id)=> setRating(id)
+ const handleClick  = ()=>{
+    setIsClicked(true)
+    setRating(rating)
+    handleRate(rating)
+ }
+ const handleOnLeave = ()=>{
+    if(!isClicked){
+          setRating(0)
+    }
+  }
+ 
+    return(
+    <Wrapper {...props} onClick={handleClick}  onMouseLeave={handleOnLeave}>
+    
+         <Star className="fa fa-star"  onMouseEnter={()=>handleHover(1)} checked={1<=rating}/>
+         <Star className="fa fa-star" onMouseEnter={()=>handleHover(2)}  checked={2<=rating}/>
+         <Star className="fa fa-star"  onMouseEnter={()=>handleHover(3)}   checked={3<=rating}/>
+         <Star className="fa fa-star" onMouseEnter={()=>handleHover(4)}   checked={4<=rating}/>
+         <Star className="fa fa-star" onMouseEnter={()=>handleHover(5)}   checked={5<=rating}/>
+    </Wrapper>
+ )
+}
+
+
+export default RateUser;
