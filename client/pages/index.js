5d492218154cda908ad9241d10a81108d55c7e8c
karam <karamkaku2000@gmail.com>
2021-10-07 16:15:02 +0200

    add profile to the main page

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,8 +4,11 @@ import Layout from '../features/common/layout/components/';
 import ProfileMarker from "../features/userMarker/components/profileMarker";
 import UserMarker from "../features/userMarker/components/userMarker";
 import NewPositionCard from '../features/userInfo/components/profileInfo/newPositionCard';
+import Profile from "../features/profile/components";
 import {useFetchQuery} from "../hooks/useFetchQuery"; 
 import {useFetchLazyQuery} from "../hooks/useFetchLazyQuery";
+import {useRouter} from "next/router";
+
 const  Index = ()=> {
   const options = {fullscreenControl: false ,disableDoubleClickZoom:true,clickableIcons: false}
   const [newGeolocation,setNewGeoLocation] = React.useState(undefined);
@@ -15,7 +18,7 @@ const  Index = ()=> {
   const [bounds,setBounds] = React.useState({ne:{lat:undefined,lng:undefined},nw:{lat:undefined,lng:undefined},sw:{lat:undefined}});
   const profile = useFetchQuery("user","http://localhost:8080/api/profile/");    
   const users = useFetchLazyQuery("users",`http://localhost:8080/api/position/users/?neLat=${bounds.ne.lat}&neLng=${bounds.ne.lng}&nwLat=${bounds.nw.lat}&nwLng=${bounds.nw.lng}&swLat=${bounds.sw.lat}`,Boolean(bounds.ne.lat))
-
+  const {query} = useRouter();
   React.useEffect(()=>{  
     navigator.geolocation.getCurrentPosition((position)=>{
       if(!profile.isLoading && profile.data.user.longitude!==position.coords.longitude&&profile.data.user.latitude!==position.coords.latitude ){
@@ -65,6 +68,7 @@ const  Index = ()=> {
     return (
       profile.isLoading?<div>isLoading....</div>:(
          <Layout data={profile.data.user}>
+        {query.id?<Profile _id={query.id}/>:null}
        <div style={{ height: '100vh', width: '100%' }}>   
           <GoogleMapReact
             bootstrapURLKeys={{ key:"AIzaSyATBu4y1OPMu1ctdhBFvBy3L1XecgDyG1k"  }}
@@ -75,7 +79,7 @@ const  Index = ()=> {
             onChange={({bounds})=>setBounds(bounds)}
            
             yesIWantToUseGoogleMapApiInternals 
-          >
+          > 
             {showNewUserPositionCard?<NewPositionCard handleCloseCard={handleCloseCard(showNewUserPositionCard,setShowNewUserPositionCard)} lat={newGeolocation?newGeolocation.latitude:null} lng={newGeolocation?newGeolocation.longitude:null} data={profile.data.user} onMouseEnter={handleOnChildEnter} onMouseLeave={handleOnChildLeave}   />:null }
             {!users.isLoading&&!users.disabledFetching?users.data.users.map(({_id,longitude,latitude})=>(
            <UserMarker  
@@ -88,7 +92,6 @@ const  Index = ()=> {
           _id={_id}
           />
          )):null}
-
            <ProfileMarker
               lat={profile.data.user.latitude}
               lng={profile.data.user.longitude}
