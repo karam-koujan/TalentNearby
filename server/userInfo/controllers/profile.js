04e948b5905eb023c6ea663a1d4486a26be57563
karam <karamkaku2000@gmail.com>
2021-10-09 00:10:22 +0200

    add getProfileReviews controller

--- server/userInfo/controllers/profile.js
+++ server/userInfo/controllers/profile.js
@@ -1,5 +1,5 @@
 const Client = require("../../model/Client");
-
+const Review = require("../../model/Review");
 
 exports.getProfile = (req,res)=>{
     res.json({
@@ -20,6 +20,24 @@ exports.getProfileById = async(req,res)=>{
     }catch{
         res.status(404).json({
             message:`this profile doesn't exist check  ${userName} spelling `,
+            error:true
+        })
+    }
+}
+exports.getProfileReviews = async(req,res,next)=>{
+    const id = req.params.id;
+    const limit = parseInt(req.query.limit);
+    const page = parseInt(req.query.page);
+    const startIndex = (page - 1)*limit;
+    try{
+      const reviews = await Review.find({talent:id}).limit(limit).skip(startIndex);
+      res.json({
+          reviews,
+          error:false
+      })
+    }catch{
+        res.status(404).json({
+            message:`this profile id does not exist`,
             error:true
         })
     }
