4cbe3da71ad51efce97159da966a03f23be4268e
karam <karamkaku2000@gmail.com>
2021-10-09 10:46:15 +0200

    add profile to userMarker

--- client/features/userMarker/components/userMarker.jsx
+++ client/features/userMarker/components/userMarker.jsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import Marker from "../../common/components/marker";
 import Styles from "../styles/styles.module.css";
 import  UserCard from "../../userInfo/components/userInfo/userCard";
-const UserMarker = ({_id,handleCloseCard,onMouseEnter,onMouseLeave,...props})=>{
+const UserMarker = ({_id,handleCloseCard,onMouseEnter,profile,onMouseLeave,...props})=>{
     const [showUserCard,setShowUserCard] = React.useState(false)
     return(
         <div className={Styles.container} {...props} >
-            {showUserCard?<UserCard _id={_id}  className={Styles.cardWrapper} handleCloseCard={handleCloseCard(showUserCard,setShowUserCard)}   onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}  />:null}
+            {showUserCard?<UserCard _id={_id} profile={profile}  className={Styles.cardWrapper} handleCloseCard={handleCloseCard(showUserCard,setShowUserCard)}   onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}  />:null}
         <Marker color="green" onClick={()=>setShowUserCard(!showUserCard)}/>
         </div>
 
