790223c81119a9178600627f37a0034c785a042d
karam <karamkaku2000@gmail.com>
2021-10-11 21:50:34 +0200

    add tool tip global class

--- client/styles/globals.js
+++ client/styles/globals.js
@@ -29,6 +29,40 @@ const GlobalStyle = createGlobalStyle`
  p{
    color:#090117;
  }
+
+ .tooltip {
+  position: relative;
+}
+
+.tooltip .tooltiptext {
+  top:-200%;
+  left:-10%;
+  visibility: hidden;
+  width: 120px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+ 
+  position: absolute;
+  z-index: 1;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+
+.tooltip .tooltiptext::after {
+  content: " ";
+  position: absolute;
+  top: 100%; 
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: black transparent transparent transparent;
+}
 `
 
 const lightTheme = {
