280ff52db020ef8b4dad8ab27601ba7aad8846db
karam <karamkaku2000@gmail.com>
2021-10-15 13:59:51 +0200

    add mainPage reducer

---
+++ client/reducers/mainPage.js
@@ -0,0 +1,17 @@
+
+export const reducer = (state,action)=>{
+    switch(action.type){
+          case "set_newGeoLocation" :
+              return {...state,newGeolocation:action.payload}
+          case "set_defaultGeoLocation" :
+              return {...state,defaultGeoLocation:action.payload}    
+          case "set_disableMapClick" :
+             return {...state,disableMapClick:action.payload}    
+          case "set_showNewUserPositionCard" :
+                 return {...state,showNewUserPositionCard:action.payload}  
+          case "set_bounds" :
+                     return {...state,bounds:action.payload}
+          case "set_showPage" :
+                         return {...state,showPage:action.payload}                          
+    }
+ }
