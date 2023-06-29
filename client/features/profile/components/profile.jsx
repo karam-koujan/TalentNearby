1e84b71f18443bf76853806e960dfb82b970b0ec
karam-koujan <karamkaku2000@gmail.com>
2023-06-29 23:52:25 +0100

    fix: remove verify email

--- client/features/profile/components/profile.jsx
+++ client/features/profile/components/profile.jsx
@@ -3,191 +3,278 @@ import * as Yup from "yup";
 import RatingStars from "../../common/components/ratingStars/";
 import Reviews from "./reviews";
 import Styles from "../styles/styles.module.css";
-import {ProfileImgWrapper,UploadImgElementsWrapper,Wrapper} from "../templates/layout";
-import {ReviewersNum, UserName,ErrorMsg} from "../templates/text";
-import { Discription,DescriptionList,DescriptionTitle } from "../templates/list";
-import {Icon,Pencil,Close, ChangeProfileImgIcon} from "../templates/icons";
-import { TextArea,Input,Button,UploadImgBtn, UpdateProfileImgBtn} from "../templates/form";
+import {
+  ProfileImgWrapper,
+  UploadImgElementsWrapper,
+  Wrapper,
+} from "../templates/layout";
+import { ReviewersNum, UserName, ErrorMsg } from "../templates/text";
+import {
+  Discription,
+  DescriptionList,
+  DescriptionTitle,
+} from "../templates/list";
+import { Icon, Pencil, Close, ChangeProfileImgIcon } from "../templates/icons";
+import {
+  TextArea,
+  Input,
+  Button,
+  UploadImgBtn,
+  UpdateProfileImgBtn,
+} from "../templates/form";
 import { useFormik } from "formik";
-import { useMutation,useQueryClient } from "react-query";
-import  {useUpdate} from "../../../hooks/httpReq/useUpdate";
+import { useMutation, useQueryClient } from "react-query";
+import { useUpdate } from "../../../hooks/httpReq/useUpdate";
 import { useUpload } from "../../../hooks/useUpload";
 import SEO from "../../common/components/SEO/user";
 
-
-const Profile = ({data:{userName,profileImg,status,bio,active,rating,reviewersNum,job,email,phoneNumber,_id}})=>{
+const Profile = ({
+  data: {
+    userName,
+    profileImg,
+    status,
+    bio,
+    active,
+    rating,
+    reviewersNum,
+    job,
+    email,
+    phoneNumber,
+    _id,
+  },
+}) => {
   const validationSchema = Yup.object({
-    phoneNumber:Yup.string(),
-    bio:Yup.string().max(300,"the maximum is 300 word")
-  })   
-  const [enableElementModification,setEnableElementModification]  = React.useState({
-        phoneNumber,
-        bio
-       })
-       const [file,setFile] = React.useState("")  
-       const [uploadedImg,setUploadedImg] = React.useState("")
-       const [hoverOnProfileImg,setHoverOnProfileImg] = React.useState(false)
-       const setUpdate = useUpdate()
-       const infoMutation = useMutation(newData=>setUpdate("http://localhost:8080/api/profile/modifyInfo",newData))
-       const profileImgMutation = useMutation(newData=>setUpdate("http://localhost:8080/api/profile/updateProfileImg",newData))
-       const client = useQueryClient()  
-       const setUpload = useUpload()
-    
-      
-    const [err,setErr] = React.useState("");
-       const {values,handleChange,handleSubmit} = useFormik({
-           initialValues : {
-               phoneNumber,
-               bio
-           },
-           validationSchema
-           ,
-           onSubmit:()=>{
-        
-             const formData = {
-                 phoneNumber:values.phoneNumber,
-                 bio:values.bio
-             }  
+    phoneNumber: Yup.string(),
+    bio: Yup.string().max(300, "the maximum is 300 word"),
+  });
+  const [enableElementModification, setEnableElementModification] =
+    React.useState({
+      phoneNumber,
+      bio,
+    });
+  const [file, setFile] = React.useState("");
+  const [uploadedImg, setUploadedImg] = React.useState("");
+  const [hoverOnProfileImg, setHoverOnProfileImg] = React.useState(false);
+  const setUpdate = useUpdate();
+  const infoMutation = useMutation((newData) =>
+    setUpdate("http://localhost:8080/api/profile/modifyInfo", newData)
+  );
+  const profileImgMutation = useMutation((newData) =>
+    setUpdate("http://localhost:8080/api/profile/updateProfileImg", newData)
+  );
+  const client = useQueryClient();
+  const setUpload = useUpload();
 
-             if(!active){
-                return setErr("please verifiy your email")
-             }
-             infoMutation.mutate(formData,{
-               onSuccess:()=>{
-                 client.invalidateQueries('user')
-               }
-             })
-             setEnableElementModification({
-                phoneNumber:true,
-                bio:true
-             })
-             window.location.reload(true)
-           }
-  })   
-  const handleUpload = async(e)=>{
-    console.log(e.target.files[0])
-        try{
-           setFile(e.target.files[0])
-           const imgUrl = await setUpload(e.target.files[0],"image");
-           console.log(imgUrl)
-           setUploadedImg(imgUrl)       
-          }catch(err){
-            console.log(err)
-        }
-  }  
- const handleUpdateProfileImg = ()=>{
-     profileImgMutation.mutate({profileImg:uploadedImg},{
-       onSuccess:()=>{
-         client.invalidateQueries("user",{exact:true})
-         location.reload()
-       }
-     })
-     setUploadedImg("")
- }
-     return(
-         <>       
-          <SEO data={{userName,profileImg,bio,job}}/>
+  const [err, setErr] = React.useState("");
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      phoneNumber,
+      bio,
+    },
+    validationSchema,
+    onSubmit: () => {
+      const formData = {
+        phoneNumber: values.phoneNumber,
+        bio: values.bio,
+      };
+
+      infoMutation.mutate(formData, {
+        onSuccess: () => {
+          client.invalidateQueries("user");
+        },
+      });
+      setEnableElementModification({
+        phoneNumber: true,
+        bio: true,
+      });
+      window.location.reload(true);
+    },
+  });
+  const handleUpload = async (e) => {
+    console.log(e.target.files[0]);
+    try {
+      setFile(e.target.files[0]);
+      const imgUrl = await setUpload(e.target.files[0], "image");
+      console.log(imgUrl);
+      setUploadedImg(imgUrl);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleUpdateProfileImg = () => {
+    profileImgMutation.mutate(
+      { profileImg: uploadedImg },
+      {
+        onSuccess: () => {
+          client.invalidateQueries("user", { exact: true });
+          location.reload();
+        },
+      }
+    );
+    setUploadedImg("");
+  };
+  return (
+    <>
+      <SEO data={{ userName, profileImg, bio, job }} />
       <Wrapper>
-          <ProfileImgWrapper profileImg={uploadedImg||profileImg} onMouseEnter={()=>setHoverOnProfileImg(true)} onMouseLeave={()=>setHoverOnProfileImg(false)}>
-            {profileImg||uploadedImg?<img loading="lazy" src={uploadedImg?uploadedImg:profileImg}  alt={`${userName} image`}/>:null}
-             {uploadedImg?(
-                    <div className={Styles.uploadImgIconsWrapper}>
-                      <UpdateProfileImgBtn onClick={()=>setUploadedImg("")}>
-                    <ChangeProfileImgIcon className="fa fa-close"></ChangeProfileImgIcon>
-                      </UpdateProfileImgBtn>
-                      <UpdateProfileImgBtn onClick={handleUpdateProfileImg}>
-                    <ChangeProfileImgIcon className="fa fa-check">
-                    </ChangeProfileImgIcon>
-                      </UpdateProfileImgBtn>
-                  </div>
-            ):null}
-             <UploadImgElementsWrapper>
-           {!uploadedImg&&hoverOnProfileImg?(
-           <UploadImgBtn>
-            <span>change profile image</span>
-            <input  type="file" className={Styles.inputFile} aria-label="change your profile image" onChange={handleUpload}/>
-          </UploadImgBtn>
-           ):null}
-             </UploadImgElementsWrapper>
-          </ProfileImgWrapper>
-          <UserName>
-           {userName}
-           </UserName>
-           {status==="talent"?(
-                 <>
-                 <RatingStars tooltip="your rating" rating={rating} margin="0 auto"  />
-                 <ReviewersNum href="#reviews">{reviewersNum} reviews</ReviewersNum>
-                 </>
-           ):null}
-           <DescriptionList>
-           {status==="talent"?(
-             <>
-             <DescriptionTitle>
-               job
-             </DescriptionTitle>
-             <Discription>
-               {job}
-             </Discription>
-             </>
-           ):null}
-         <DescriptionTitle>
-               Phone
-             </DescriptionTitle>
-            
-                 {enableElementModification.phoneNumber?(
-                 <div style={{'display':'flex'}}>
-                 <Discription>
-            <Icon className="fa fa-phone"></Icon>
-              {phoneNumber}
-            </Discription>
-            <Pencil className="fa fa-pencil" onClick={()=>setEnableElementModification({...enableElementModification,phoneNumber:false})}></Pencil>
-                </div>
-             ):( 
-               <div className={Styles.inputWrapper}>
-                 <form  onSubmit={handleSubmit}>
-                 <Input  type="tel" placeholder="phone number" name="phoneNumber" value={values.phoneNumber}  onChange={handleChange}/>
-                 {err?<ErrorMsg>{err}</ErrorMsg>:null}
-                 </form>
-             {phoneNumber?<Close className={Styles.close} onClick={()=>setEnableElementModification(prev=>({...prev,phoneNumber:true}))}>&#10006;</Close>:null}
-                </div>     
-                 )}
-             <DescriptionTitle>
-               Email
-             </DescriptionTitle>
-             <Discription>
-              <Icon className="fa fa-envelope"></Icon>
-              {email}
-             </Discription>
-             <DescriptionTitle>
-              bio
-             </DescriptionTitle>
-               {enableElementModification.bio?(
-                 <div style={{'display':'flex'}}>
-            <Discription>
-               {bio}
-            </Discription>
-               <Pencil className="fa fa-pencil" onClick={()=>setEnableElementModification({...enableElementModification,bio:false})}></Pencil>
-                 </div>
-             ):(
-                 <div className={Styles.inputWrapper}>
-                   <form  onSubmit={handleSubmit}>
-                     <TextArea placeholder="write something about you..." name="bio" onChange={handleChange} value={values.bio} />
-                     {err?<ErrorMsg>{err}</ErrorMsg>:null}
-                   </form>
-                    {bio ? <Close className={Styles.close} onClick={()=>setEnableElementModification(prev=>({...prev,bio:true}))}>&#10006;</Close>:null}
-                 </div>
-             )}
-             
-           </DescriptionList>
-            {!enableElementModification.bio||!enableElementModification.phoneNumber?(
-            <Button type="submit" name="modify your information" onClick={handleSubmit} >
-                 submit
-             </Button>):null} 
-           {status==="talent"?<Reviews profileId={_id}/>:null}
-  
-        
+        <ProfileImgWrapper
+          profileImg={uploadedImg || profileImg}
+          onMouseEnter={() => setHoverOnProfileImg(true)}
+          onMouseLeave={() => setHoverOnProfileImg(false)}
+        >
+          {profileImg || uploadedImg ? (
+            <img
+              loading="lazy"
+              src={uploadedImg ? uploadedImg : profileImg}
+              alt={`${userName} image`}
+            />
+          ) : null}
+          {uploadedImg ? (
+            <div className={Styles.uploadImgIconsWrapper}>
+              <UpdateProfileImgBtn onClick={() => setUploadedImg("")}>
+                <ChangeProfileImgIcon className="fa fa-close"></ChangeProfileImgIcon>
+              </UpdateProfileImgBtn>
+              <UpdateProfileImgBtn onClick={handleUpdateProfileImg}>
+                <ChangeProfileImgIcon className="fa fa-check"></ChangeProfileImgIcon>
+              </UpdateProfileImgBtn>
+            </div>
+          ) : null}
+          <UploadImgElementsWrapper>
+            {!uploadedImg && hoverOnProfileImg ? (
+              <UploadImgBtn>
+                <span>change profile image</span>
+                <input
+                  type="file"
+                  className={Styles.inputFile}
+                  aria-label="change your profile image"
+                  onChange={handleUpload}
+                />
+              </UploadImgBtn>
+            ) : null}
+          </UploadImgElementsWrapper>
+        </ProfileImgWrapper>
+        <UserName>{userName}</UserName>
+        {status === "talent" ? (
+          <>
+            <RatingStars
+              tooltip="your rating"
+              rating={rating}
+              margin="0 auto"
+            />
+            <ReviewersNum href="#reviews">{reviewersNum} reviews</ReviewersNum>
+          </>
+        ) : null}
+        <DescriptionList>
+          {status === "talent" ? (
+            <>
+              <DescriptionTitle>job</DescriptionTitle>
+              <Discription>{job}</Discription>
+            </>
+          ) : null}
+          <DescriptionTitle>Phone</DescriptionTitle>
+
+          {enableElementModification.phoneNumber ? (
+            <div style={{ display: "flex" }}>
+              <Discription>
+                <Icon className="fa fa-phone"></Icon>
+                {phoneNumber}
+              </Discription>
+              <Pencil
+                className="fa fa-pencil"
+                onClick={() =>
+                  setEnableElementModification({
+                    ...enableElementModification,
+                    phoneNumber: false,
+                  })
+                }
+              ></Pencil>
+            </div>
+          ) : (
+            <div className={Styles.inputWrapper}>
+              <form onSubmit={handleSubmit}>
+                <Input
+                  type="tel"
+                  placeholder="phone number"
+                  name="phoneNumber"
+                  value={values.phoneNumber}
+                  onChange={handleChange}
+                />
+                {err ? <ErrorMsg>{err}</ErrorMsg> : null}
+              </form>
+              {phoneNumber ? (
+                <Close
+                  className={Styles.close}
+                  onClick={() =>
+                    setEnableElementModification((prev) => ({
+                      ...prev,
+                      phoneNumber: true,
+                    }))
+                  }
+                >
+                  &#10006;
+                </Close>
+              ) : null}
+            </div>
+          )}
+          <DescriptionTitle>Email</DescriptionTitle>
+          <Discription>
+            <Icon className="fa fa-envelope"></Icon>
+            {email}
+          </Discription>
+          <DescriptionTitle>bio</DescriptionTitle>
+          {enableElementModification.bio ? (
+            <div style={{ display: "flex" }}>
+              <Discription>{bio}</Discription>
+              <Pencil
+                className="fa fa-pencil"
+                onClick={() =>
+                  setEnableElementModification({
+                    ...enableElementModification,
+                    bio: false,
+                  })
+                }
+              ></Pencil>
+            </div>
+          ) : (
+            <div className={Styles.inputWrapper}>
+              <form onSubmit={handleSubmit}>
+                <TextArea
+                  placeholder="write something about you..."
+                  name="bio"
+                  onChange={handleChange}
+                  value={values.bio}
+                />
+                {err ? <ErrorMsg>{err}</ErrorMsg> : null}
+              </form>
+              {bio ? (
+                <Close
+                  className={Styles.close}
+                  onClick={() =>
+                    setEnableElementModification((prev) => ({
+                      ...prev,
+                      bio: true,
+                    }))
+                  }
+                >
+                  &#10006;
+                </Close>
+              ) : null}
+            </div>
+          )}
+        </DescriptionList>
+        {!enableElementModification.bio ||
+        !enableElementModification.phoneNumber ? (
+          <Button
+            type="submit"
+            name="modify your information"
+            onClick={handleSubmit}
+          >
+            submit
+          </Button>
+        ) : null}
+        {status === "talent" ? <Reviews profileId={_id} /> : null}
       </Wrapper>
     </>
-    )
-}
-export default Profile ;
+  );
+};
+export default Profile;
--- client/features/rateUser/index.jsx
+++ client/features/rateUser/index.jsx
@@ -1,67 +1,81 @@
 import * as React from "react";
 import RatingStars from "../common/components/ratingStars";
 import { Heading } from "./template/heading";
-import {Wrapper,Modal} from "./template/layout";
+import { Wrapper, Modal } from "./template/layout";
 import { ErrMsg } from "./template/text";
-import {Form,TextArea,Label,Button} from "./template/form";
-import {useFormik} from "formik";
-import {useUpdate} from "../../hooks/httpReq/useUpdate";
-import {Close} from "./template/icons";
-import {useMutation, useQueryClient} from "react-query";
+import { Form, TextArea, Label, Button } from "./template/form";
+import { useFormik } from "formik";
+import { useUpdate } from "../../hooks/httpReq/useUpdate";
+import { Close } from "./template/icons";
+import { useMutation, useQueryClient } from "react-query";
 import { Router, useRouter } from "next/router";
-const RateUser = ({userName,profile:{active},userId,...props})=>{
-  const [rating,setRating] = React.useState(0)
-  const [err,setErr] = React.useState("")
+const RateUser = ({ userName, profile: { active }, userId, ...props }) => {
+  const [rating, setRating] = React.useState(0);
+  const [err, setErr] = React.useState("");
   const setUpdate = useUpdate();
-  const queryClient = useQueryClient()
-  const mutation = useMutation(newData=>setUpdate("http://localhost:8080/api/rate/",newData))
-  const router = useRouter()
-  const handleRate = (rating)=>{
-       setRating(rating)
-  }
-  const {values,handleChange,handleSubmit} = useFormik({
-     initialValues:{
-       review:''
-     },
-     onSubmit :async()=>{
-      try{
-           if(!active){
-             setErr("please verifiy your email")
-           }
-           mutation.mutate({text:values.review,talentId:userId,rating},{
-             onSuccess:()=>{
-              queryClient.invalidateQueries(["users",userId])
-              queryClient.invalidateQueries(["reviews",userId])
-             }
-           })
-          
-           router.replace("/",undefined,{shallow:true})
-        }catch(err){
-        console.log(err)
+  const queryClient = useQueryClient();
+  const mutation = useMutation((newData) =>
+    setUpdate("http://localhost:8080/api/rate/", newData)
+  );
+  const router = useRouter();
+  const handleRate = (rating) => {
+    setRating(rating);
+  };
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      review: "",
+    },
+    onSubmit: async () => {
+      try {
+        mutation.mutate(
+          { text: values.review, talentId: userId, rating },
+          {
+            onSuccess: () => {
+              queryClient.invalidateQueries(["users", userId]);
+              queryClient.invalidateQueries(["reviews", userId]);
+            },
+          }
+        );
+
+        router.replace("/", undefined, { shallow: true });
+      } catch (err) {
+        console.log(err);
       }
-     }
-  })
-  return(
+    },
+  });
+  return (
     <Wrapper>
-    <Modal {...props}>
-      {err?<ErrMsg>{err}</ErrMsg>:null}
-      {!active?<ErrMsg>you cannot rate user without verifying your Email</ErrMsg>:null}
-      <Close onClick={()=>router.replace("/",undefined,{shallow:true})}>&#10006;</Close>
-        <Heading>
-            Rate {userName}
-        </Heading>
-        <RatingStars margin="0 auto" handleRate={handleRate} enableRating/>
-        <Form  onSubmit={handleSubmit}>
+      <Modal {...props}>
+        {err ? <ErrMsg>{err}</ErrMsg> : null}
+        {!active ? (
+          <ErrMsg>you cannot rate user without verifying your Email</ErrMsg>
+        ) : null}
+        <Close
+          onClick={() => router.replace("/", undefined, { shallow: true })}
+        >
+          &#10006;
+        </Close>
+        <Heading>Rate {userName}</Heading>
+        <RatingStars margin="0 auto" handleRate={handleRate} enableRating />
+        <Form onSubmit={handleSubmit}>
           <Label>review</Label>
-          <TextArea placeholder="Bio" name="review" value={values.review} onChange={handleChange}/>
-        <Button type="submit"  onSubmit={handleSubmit} disabled={!values.review}>Review</Button>
+          <TextArea
+            placeholder="Bio"
+            name="review"
+            value={values.review}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            onSubmit={handleSubmit}
+            disabled={!values.review}
+          >
+            Review
+          </Button>
         </Form>
-    </Modal>
+      </Modal>
     </Wrapper>
-  )
-}
+  );
+};
 
 export default RateUser;
-
-
-
